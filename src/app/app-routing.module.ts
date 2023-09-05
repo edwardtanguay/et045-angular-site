2e131a135608a9216a020383b5b650141d48690f
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-05 15:16:29 +0200

    two pages work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'todo-list', component: TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/menu/menu.config.ts
@@ -0,0 +1,15 @@
+import { Routes } from "@angular/router";
+import { WelcomeComponent } from "../welcome/welcome.component";
+import { TodoListComponent } from "../todo-list/todo-list.component";
+import { AngularInfoComponent } from "../angular-info/angular-info.component";
+
+export const routerConfig: Routes = [
+	{
+		path: 'welcome',
+		component: WelcomeComponent
+	},
+	{
+		path: 'todo-list',
+		component: TodoListComponent
+	}
+]
