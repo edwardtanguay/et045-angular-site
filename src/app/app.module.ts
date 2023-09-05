71056cd8e3ee1f9ace49233989000dae6a8c5ea0
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-05 16:02:57 +0200

    custom pipe works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +7,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AngularInfoComponent } from './angular-info/angular-info.component';
 import { MenuComponent } from './menu/menu.component';
+import { NumtonamePipe } from '../shared/numtoname.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { MenuComponent } from './menu/menu.component';
     TodoListComponent,
     WelcomeComponent,
     AngularInfoComponent,
-    MenuComponent
+    MenuComponent,
+    NumtonamePipe
   ],
   imports: [
     BrowserModule,
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
-
+  section001 = 'section 1';
+  section002 = 'section 2';
+  section003 = 'section 3';
 }
