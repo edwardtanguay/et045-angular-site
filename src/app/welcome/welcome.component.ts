a0b5b29eb9d6a0d903fe583b52068d8d21920f9a
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-05 16:08:00 +0200

    ngFor in

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+	selector: 'app-welcome',
+	templateUrl: './welcome.component.html',
+	styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
-  section001 = 'section 1';
-  section002 = 'section 2';
-  section003 = 'section 3';
+	section001 = 'section 1';
+	section002 = 'section 2';
+	section003 = 'section 3';
+
+	colors = ['red', 'blue', 'green', 'yellow', 'purple'];
 }
